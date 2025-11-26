063243ce1eb483aed7a7cf80de8ed83e54240cf1
Csombo-Varga Benedek <b1csoben@vasvari.org>
2025-11-26 09:31:59 +0100

    hivasok starterd

---
+++ IO feladatok/37 - Hivasok/Hivasok/Hivasok/Hivas.cs
@@ -0,0 +1,19 @@
+﻿namespace Hivasok;
+
+public class Hivas
+{
+    public int StartHour { get; set; }
+    public int StartMinute { get; set; }
+    public int StartSecond { get; set; }
+
+    public int EndHour { get; set; }
+    public int EndMinute { get; set; }
+    public int EndSecond { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public override string ToString()
+    {
+        return $"{StartHour:D2}:{StartMinute:D2}:{StartSecond:D2} - {EndHour:D2}:{EndMinute:D2}:{EndSecond:D2} : {PhoneNumber}";
+    }
+}
---
+++ IO feladatok/37 - Hivasok/Hivasok/Hivasok/Program.cs
@@ -0,0 +1,19 @@
+﻿var numbers = File.ReadAllLines("hivasok.txt")
+                  .Where(x=> x.Length == 9)
+                  .Select(x => int.Parse(x))
+                  .ToList();
+
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+var durations = File.ReadAllLines("hivasok.txt")
+                     .Where(x => x.Length > 9)
+                     .Select(x => TimeSpan.ParseExact(x, "hhmmss", null))
+                     .ToList();
+
+foreach (var duration in durations)
+{
+    Console.WriteLine(duration);
+}
