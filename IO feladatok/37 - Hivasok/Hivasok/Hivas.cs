91ac45ca9375ab657c16c0633140f0e85b5f7864
Midvincible <117970056+beniiiiiiiii@users.noreply.github.com>
2026-02-11 23:44:02 +0100

    Add files via upload

---
+++ IO feladatok/37 - Hivasok/Hivasok/Hivas.cs
@@ -0,0 +1,29 @@
+﻿namespace Hivasok;
+
+class Hivas
+{
+    public TimeSpan Kezdet { get; set; }
+    public TimeSpan Vege { get; set; }
+    public string Telefonszam { get; set; }
+
+    public int SzamlazottPercek()
+    {
+        double percek = (Vege - Kezdet).TotalMinutes;
+        return (int)Math.Ceiling(percek);
+    }
+
+    public bool Csucsido()
+    {
+        TimeSpan csucsStart = new TimeSpan(7, 0, 0);
+        TimeSpan csucsVege = new TimeSpan(18, 0, 0);
+
+        return Kezdet >= csucsStart && Kezdet < csucsVege;
+    }
+
+    public bool Mobil()
+    {
+        return Telefonszam.StartsWith("39") ||
+               Telefonszam.StartsWith("41") ||
+               Telefonszam.StartsWith("71");
+    }
+}
---
+++ IO feladatok/37 - Hivasok/Hivasok/Program.cs
@@ -0,0 +1,70 @@
+﻿using Hivasok;
+
+var sorok = File.ReadAllLines("hivasok.txt");
+var hivasok = new List<Hivas>();
+
+for (int i = 0; i < sorok.Length; i += 2)
+{
+    var ido = sorok[i].Split().Select(int.Parse).ToArray();
+    hivasok.Add(new Hivas
+    {
+        Kezdet = new TimeSpan(ido[0], ido[1], ido[2]),
+        Vege = new TimeSpan(ido[3], ido[4], ido[5]),
+        Telefonszam = sorok[i + 1]
+    });
+}
+
+Console.Write("Adjon meg egy telefonszámot: ");
+string szam = Console.ReadLine();
+
+bool mobil = szam.StartsWith("39") ||
+             szam.StartsWith("41") ||
+             szam.StartsWith("71");
+
+Console.WriteLine(mobil ? "Mobil szám" : "Vezetékes szám");
+Console.WriteLine();
+
+Console.Write("Kezdő idő (óra perc mp): ");
+var k = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Console.Write("Vége idő (óra perc mp): ");
+var v = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+var kezdet = new TimeSpan(k[0], k[1], k[2]);
+var vege = new TimeSpan(v[0], v[1], v[2]);
+
+int percek = (int)Math.Ceiling((vege - kezdet).TotalMinutes);
+Console.WriteLine($"Számlázott percek: {percek}");
+Console.WriteLine();
+
+File.WriteAllLines("percek.txt",
+    hivasok.Select(h => $"{h.SzamlazottPercek()} {h.Telefonszam}"));
+
+int csucs = hivasok.Count(h => h.Csucsido());
+int nemCsucs = hivasok.Count - csucs;
+
+Console.WriteLine($"Csúcsidőben indított hívások: {csucs}");
+Console.WriteLine($"Csúcsidőn kívüli hívások: {nemCsucs}");
+Console.WriteLine();
+
+int mobilPercek = hivasok
+    .Where(h => h.Mobil())
+    .Sum(h => h.SzamlazottPercek());
+
+int vezetekesPercek = hivasok
+    .Where(h => !h.Mobil())
+    .Sum(h => h.SzamlazottPercek());
+
+Console.WriteLine($"Mobil percek: {mobilPercek}");
+Console.WriteLine($"Vezetékes percek: {vezetekesPercek}");
+Console.WriteLine();
+
+double osszeg = hivasok
+    .Where(h => h.Csucsido())
+    .Sum(h =>
+    {
+        double ar = h.Mobil() ? 69.175 : 30;
+        return ar * h.SzamlazottPercek();
+    });
+
+Console.WriteLine($"Csúcsdíjas hívások összege: {osszeg:F2} Ft");
